aba6776ca4963d9a280d09ca9023caf11585c988
spnab <nicholas-a-becker@protonmail.com>
2025-04-03 22:53:00 -0500

    tests

--- py/qualifiers.py
+++ py/qualifiers.py
@@ -9,6 +9,7 @@ Wildcards:
                      -> qualifier, otherwise it's just functioning as a hyphen. 
 .-.                  -> Matches 0-22 qualifiers
 """
+import re
 import unittest
 
 def specific_pattern(pattern1, pattern2):
@@ -33,30 +34,47 @@ def specific_pattern(pattern1, pattern2):
   for c in ''.join(quals1 + quals2):
     assert c in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$-*'
 
-  # specific
+  # convert patterns to regular expressions
 
   # return
   return result
 
-def specific_qualifier(qual1, qual2):
-  """
-  Find the specific qualifier that matches both qual1 and qual2.
-  """
-  i = 0
-  j = 0
-
-  # first character is -
-  if qual1[0] == '-':
-
-  elif qual2[0] == '-':
-
-  # last character is -
-
 class Tests(unittest.TestCase):
   """
+  Unit tests.
   """
   def test1(self):
     p1 = 'AB.CD'
     p2 = 'EF.GH'
     r = specific_pattern(p1, p2)
     assert r == ""
+
+  def test2(self):
+    p1 = 'NICK.BECKER'
+    p2 = 'N-.B-'
+    r = specific_pattern(p1, p2)
+    assert r == 'NICK.BECKER'
+
+  def test3(self):
+    p1 = 'NICK.-.BEC-ER'
+    p2 = '*ICK.MOTHER.TRUCKING.BEC-ER'
+    r = specific_pattern(p1, p2)
+    assert r == 'NICK.MOTHER.TRUCKING.BEC-ER'
+
+  def test4(self):
+    """
+    This is a nice test. Thanks, @acf2sme from Twitch.
+    """
+    p1 = '*-.-'
+    p2 = 'NICK.I-.-'
+    p3 = 'N-.*S.SLOW'
+    r12 = specific_pattern(p1, p2)
+    assert r12 == 'NICK.I-.-'
+    r13 = specific_pattern(p1, p3)
+    assert r13 == 'N-.*S.SLOW'
+    r23 = specific_pattern(p2, p3)
+    assert r23 == 'NICK.IS.SLOW'
+    r1213 = specific_pattern(r12, r13)
+    r1223 = specific_pattern(r12, r23)
+    r1323 = specific_pattern(r13, r23)
+    assert r1213 == r1223 == r1323 == 'NICK.IS.SLOW'
